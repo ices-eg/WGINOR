8b4277a9c4fd035837f8945bdebbda6410fb94eb
Planque, Benjamin <benjamin.planque@hi.no>
2022-10-18 08:59:53 +0200

    Export to png in report script

--- TAF_NPP/report.R
+++ TAF_NPP/report.R
@@ -3,7 +3,7 @@
 # libraries --------------------------------------------
 library(icesTAF)
 require(tidyverse)
-mkdir("output")
+mkdir("report")
 
 # load model results ------------------------------------------------------
 NPP.series <- read.taf(file.path(".","model","NPPseries.txt"))
@@ -11,6 +11,7 @@ NPP.annual <- read.taf(file.path(".","model","NPPannual.txt"))
 
 # Figures -----------------------------------------------------------------
 # 1 - multiple panel plot (individual years) with 8-day observations and hierarchical model 
+taf.png(file.path(".","report","IndividualYears.png"),width =2100, height =1485)
 ggplot() +
   geom_path(aes(x=doy,y=NPP.pred0),data = NPP.series, size = 0.5, col = "deepskyblue") +
   geom_path(aes(x=doy,y=NPP.pred),data = NPP.series, size = 1, col = "firebrick") +
@@ -20,8 +21,10 @@ ggplot() +
   #geom_abline(intercept = c(start_threshold,end_threshold),slope=0,lty=2) +
   facet_wrap(~year,ncol=5) +
   ylab(expression(paste("NPP (mg.C.",m^-2,".",day^-1,")",sep="")))
+dev.off()
 
 # 2 - matrix type plot with start, peak and end of production season
+taf.png(file.path(".","report","MatrixNPP.png"),width =1250, height =1485)
 ggplot(data=NPP.series) +
   geom_tile(aes(x=doy,y=year,fill=NPP.mean)) +
   geom_path(aes(x=peak,y=year),data=NPP.annual,size=1,col="black") +
@@ -29,10 +32,13 @@ ggplot(data=NPP.series) +
   geom_path(aes(x=end,y=year),data=NPP.annual,col="black") +
   scale_fill_gradient(low="lightyellow",high = "seagreen") +
   labs(fill=expression(paste("NPP (mg.C.",m^-2,".",day^-1,")",sep="")))
+dev.off()
 
 # 3 - barplot with the NPP integrated over the observation season
+taf.png(file.path(".","report","AnnualNPP.png"),width =1250, height =700)
 ggplot(data=NPP.annual,aes(x=year,y=totNPP)) +
   geom_bar(stat = "identity") +
   ylim(c(0,NA)) +
   ylab(expression(paste("g.C.",m^-2,".",y^-1,sep="")))
+dev.off()
 
