96ec4750eb3f91bb2db7459d3f41dd0bba3686d6
Planque, Benjamin <benjamin.planque@hi.no>
2025-11-07 08:01:57 +0100

    Update with 2025 dataset

--- TAF_ATAC/model.R
+++ TAF_ATAC/model.R
@@ -12,15 +12,18 @@ sourceTAF("utilities")
 
 mkdir("model")
 
-#load data
-data <- read.taf("data") #load data (list of the different data tables)
+# load data
+data <- read.taf("data") # load data (list of the different data tables)
 series <- as_tibble(data$series)
 info <- as_tibble(data$info)
 
 # run model
-for (ycol in c(2:dim(series)[2])){ # loop on all time-series 
-    inputseries <- series %>% transmute(x=Year,y=.[,ycol][[1]]) %>% filter(is.na(y)==0) %>% as_tibble() # wrangle data prior to modelling
-  ATAC.fit <- try(ATAC(inputseries,year_start=1980,year_end_OL=2021,n_forecasts = 3),TRUE) # fit models to an individual time series
-  save(ATAC.fit, file = file.path(".","model",paste0("modelfits",ycol,".Rdata"))) # save each individual model
+for (ycol in c(2:dim(series)[2])) { # loop on all time-series
+  #  for (ycol in c(4,8,9,14:23)){ # loop on Zoopk time-series
+  inputseries <- series %>%
+    transmute(x = Year, y = .[, ycol][[1]]) %>%
+    filter(is.na(y) == 0) %>%
+    as_tibble() # wrangle data prior to modelling
+  ATAC.fit <- try(ATAC(inputseries, year_start = 1980, year_end_OL = 2022, n_forecasts = 3), TRUE) # fit models to an individual time series
+  save(ATAC.fit, file = file.path(".", "model", paste0("modelfits", ycol, ".Rdata"))) # save each individual model
 }
-
